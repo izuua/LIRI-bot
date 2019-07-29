5720d791266872bc4cee95da27af935bf65a3f49
izuua <izuuaaf@gmail.com>
2019-07-28 23:45:27 -0400

    Updated the movie call function.

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 LIRI will search Spotify for songs, Bands in Town for concerts, and OMDB for movies.
 
+It takes user input and uses axios calls to get data.
+
+Input one of 4 commands: "concert-this [band name]", "spotify-this-song [song name]", "movie-this [movie name]", or "do-what-it-says"
--- liri.js
+++ liri.js
@@ -3,3 +3,107 @@ var keys = require("./keys.js");
 
 var spotify = new Spotify(keys.spotify)
 
+var axios = require("axios");
+
+function concertCall() {
+    var artistName = process.argv.slice(3).join("%20")
+
+    axios.get("https://rest.bandsintown.com/artists/" + artistName + "/events?app_id=codingbootcamp").then(
+        function (response) {
+            // venue name
+            // venue location
+            // date
+
+        }).catch(function (error) {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log("---------------Data---------------");
+                console.log(error.response.data);
+                console.log("---------------Status---------------");
+                console.log(error.response.status);
+                console.log("---------------Status---------------");
+                console.log(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        })
+}
+
+function spotifyCall() {
+    var songName = process.argv.slice(3).join("%20")
+    if (!songName) {
+        songName = "The%20Sign";
+    }
+
+    spotify.search({ type: 'track', query: songName }, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+       
+      console.log(data); 
+      });
+}
+
+function movieCall() {
+    var movieName = process.argv.slice(3).join("%20")
+    if (!movieName) {
+        movieName = "Mr%20Nobody";
+    }
+    axios.get("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=4859707d").then(
+        function (response) {
+            console.log(`Title: ${response.data.Title || "undefined"}`);
+            console.log(`Release Date: ${response.data.Released || "undefined"}`);
+            console.log(`IMDB rating: ${response.data.Ratings[0].Value || "undefined"}`);
+            console.log(`Rotten Tomatoes rating: ${response.data.Ratings[1].Value || "undefined"}`);
+            console.log(`Country: ${response.data.Country || "undefined"}`);
+            console.log(`Language: ${response.data.Language || "undefined"}`);
+            console.log(`Plot: ${response.data.Plot || "undefined"}`);
+            console.log(`Actors: ${response.data.Actors || "undefined"}`);
+
+        }).catch(function (error) {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log("---------------Data---------------");
+                console.log(error.response.data);
+                console.log("---------------Status---------------");
+                console.log(error.response.status);
+                console.log("---------------Status---------------");
+                console.log(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        })
+}
+
+switch (process.argv[2].toLowerCase()) {
+    case "concert-this":
+        console.log(process.argv[2])
+        concertCall();
+        break;
+    case "spotify-this-song":
+        console.log(process.argv[2])
+        break;
+    case "movie-this":
+        console.log(process.argv[2])
+        movieCall();
+        break;
+    case "do-what-it-says":
+        console.log(process.argv[2])
+        break;
+    default:
+        console.log("No command chosen");
+}
